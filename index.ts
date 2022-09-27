333042a98b802f49e1edb35224731f6f7db70f0b
JHuaWei <2237268069@qq.com>
2022-09-27 15:49:07 +0800

    匹配任意文件

--- index.ts
+++ index.ts
@@ -10,33 +10,17 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const { method, url: path, headers } = request
     console.log(path)
     const { pathname, search } = url.parse(path as string)
-    switch (pathname) {
-        case '/index.html':
-            response.setHeader('Content-Type', 'text/html;charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString())
-            })
-            break
-        case '/style.css':
-            response.setHeader('Content-Type', 'text/css;charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString())
-            })
-            break
-        case '/main.js':
-            response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-
-            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString())
-            })
-            break
-        default:
+    // response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    const filename = pathname?.substring(1);
+    fs.readFile(p.resolve(publicDir, filename as string), (error, data) => {
+        if (error) {
             response.statusCode = 404
-            response.end()
-    }
+            response.end('你要的文件不存在')
+        } else {
+            response.end(data.toString())
+        }
+
+    })
 }),
 
 
